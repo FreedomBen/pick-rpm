d5317e3f154a631635be1024cf112dee668b15fe
Benjamin Porter <FreedomBen@users.noreply.github.com>
2020-04-28 15:54:55 -0600

    Add version check script

--- build.sh
+++ build.sh
@@ -18,14 +18,47 @@ ensure_args ()
     die "Second arg empty.  Should be distro version:  31, 32, 8.1, 8.2"
   fi
 
-  if [ -z "$3" ]; then
+  if [ -z "$3" ] || ! [[ $3 =~ ^(latest|[0-9]\.[0-9]\.[0-9])$ ]]; then
     die "Third arg empty.  Should be pick version or 'latest'"
   fi
+}
+
+latest_pick_version ()
+{
+  curl https://github.com/mptre/pick/releases/latest \
+    | awk -F '<' '{ print $4 }' \
+    | sed -e 's/.*v//g' \
+    | sed -e 's/".*//g'
+}
+
+is_valid_pick_version ()
+{
+  curl -L -I "https://github.com/mptre/pick/archive/v${1}.tar.gz" \
+    | grep -E '^HTTP.*200 OK' \
+    > /dev/null \
+    2>&1
+}
+
+set_args ()
+{
+  if [ "$1" = "latest" ]; then
+    export PICK_VERSION="$(latest_pick_version)"
+    echo "Latest pick version identified as '$PICK_VERSION'"
+  elif is_valid_pick_version "$1"; then
+    export PICK_VERSION="$1"
+    echo "Using pick version '$PICK_VERSION'"
+  else
+    die "Pick version does not appear valid.  Can you double check?"
+  fi
+  export PICK_VERSION_TARBALL="v${PICK_VERSION}.tar.gz"
+  export PICK_VERSION_SPEC_FILE="pick-v${PICK_VERSION}.spec"
 }
 
 main ()
 {
   ensure_args "$@"
+
+  set_args "$3"
 
   # Build container image
   podman build \
