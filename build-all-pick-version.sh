e5b784595611f7d7886c7db6bafbdd8e23a47f30
Benjamin Porter <FreedomBen@users.noreply.github.com>
2020-11-18 17:49:12 -0700

    Add Fedora 33 to build scripts

--- build-all-pick-version.sh
+++ build-all-pick-version.sh
@@ -3,6 +3,6 @@
 if [ -z "$1" ]; then
   echo "Must pass pick version as first arg"
 else
-  parallel ./build.sh fedora {} "$1" ::: 30 31 32
+  parallel ./build.sh fedora {} "$1" ::: 30 31 32 33
   parallel ./build.sh centos {} "$1" ::: 8
 fi
