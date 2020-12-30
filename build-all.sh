6c9b4f38c23dbce63a729a798e97e8fe64251847
Benjamin Porter <FreedomBen@users.noreply.github.com>
2020-12-29 17:28:45 -0700

    Drop F30, F31, add F34

--- build-all.sh
+++ build-all.sh
@@ -4,7 +4,8 @@ serial_build ()
 {
   for pick_version in 4.0.0 3.0.1; do
     # Fedora RPMs
-    for distro_version in 30 31 32 33; do
+    #for distro_version in 30 31 32 33 34; do
+    for distro_version in 32 33 34; do
       ./build.sh fedora "$distro_version" "$pick_version"
     done
 
